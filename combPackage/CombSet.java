1b50d96c956afdbc6b6e82891a6120fca4ff24fd
taogaede <tao.gaede@live.ca>
2020-06-18 10:43:28 -0700

    Updated readme.md and CombSet subclass Components summary

--- combPackage/CombSet.java
+++ combPackage/CombSet.java
@@ -81,6 +81,47 @@ public class CombSet extends ArrayList<Object>{
 	}
 }
 /*
+ * CombSet subclasses below.
+ * Each newly defined CombSet subclass must have the following components:
+ * 
+ * 1.	Private initialSet CombSet field.  
+ * 		The initial set is a CombSet from which your construction algorithm begins
+ * 		
+ * 2.	Private initialSetDescription String field.  
+ * 		This is a one sentence description of exactly what is contained within the initial set.
+ * 		Abstract set notation can also be used as in: {positive integers in [2^(n-1), 2^(n) - 1], where n is also a positive integer}.
+ * 
+ * 3.	Private constructingFunction Function field.  
+ * 		Each step in the construction algorithm will be a less abstract function element in constructingFunction's rule[] array.
+ * 		It will be within the CombSet subclass constructor that the rule[] array elements will be defined.
+ * 
+ * 4.	Private setDescription String field.  
+ * 		This field is a one sentence abstract description of exactly what elements are contained within the resulting set of the construction algorithm.
+ * 
+ * 5.	Private algorithmSource String field.
+ * 		This field contains the name of both the author of the algorithm used, as well as a literature, or other acceptable reference source for the algorithm.
+ * 
+ * 6.	Public constructor(s).
+ * 		The constructor method is where the actual construction algorithm takes place, and the manner by which it takes place is as follows:
+ * 		1.	Initialize the initialSet.
+ *		2.	Build the constructingFunction, one rule at a time:
+ *				Step 1.	constructingFunction = new Function(initialSet, new Function(new FirstOperation()));
+ *				Step 2.	constructingFunction.addRule(new Function(new SecondOperation()));
+ *						...
+ *				Step N.	constructingFunction.addRule(new Function(new NthOperation()));
+ *		3.	Add the elements to the constructed CombSet using the for loop shown in the below template.
+ *
+ * 7.	Private supporter methods (if needed).
+ * 		If you need any additional methods to assist in initializing the initialSet or for building the constructingFunction, then add them here.
+ * 		Try to avoid making supporter methods that transform the elements of the set being constructed, because such behavior should be performed by an Operation subclass, which is then called here.
+ * 		If you find that you're building with code that isn't centered around "constructingFunction.addRule(new Function(new SomeOperation()));" 
+ * 		then it may be the case that what your writing could be better done by some new operation subclass.
+ * 
+ * 8.	Public printDescription method.
+ * 		This method overrides a CombSet method.  This method prints all the relevant details your new CombSet subclass' construction.
+ * 		You can use the template's printDescription method as is, unless you would like to add or remove anything.  
+ * 
+
 //					BEGIN TEMPLATE
 
 class YourCombSet extends CombSet{
