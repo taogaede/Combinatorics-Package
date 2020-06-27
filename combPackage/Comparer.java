f039aed65761e42710f1489db495eafac3fc1081
Alexander Bird <alex@alexanderbird.software>
2020-06-27 11:32:01 -0700

    Add unit tests to compare sequences & matrices; simplify Comparer

--- combPackage/Comparer.java
+++ combPackage/Comparer.java
@@ -1,118 +1,21 @@
 package combPackage;
 
+import java.util.Arrays;
+
 public class Comparer {
-	private boolean isEqual;
-	private String first;
-	private String Second;
+	private final Object first;
+	private final Object second;
 	
-	public Comparer() {
-		
+	public Comparer(Object first, Object second) {
+		this.first = first;
+		this.second = second;
 	}
-	public Comparer(Object first, Object second) {	
 
-		//Sequences
-		if (first instanceof Integer[] && second instanceof Integer[]) {
-			this.isEqual = equals( (Integer[]) first, (Integer[]) second);
-		}
-		if (first instanceof Double[] && second instanceof Double[]) {
-			this.isEqual = equals( (Double[]) first, (Double[]) second);
-		}
-		if (first instanceof String[] && second instanceof String[]) {
-			this.isEqual = equals( (String[]) first, (String[]) second);
-		}
-		
-		//Matrices
-		if (first instanceof Integer[][] && second instanceof Integer[][]) {
-			this.isEqual = equals( (Integer[][]) first, (Integer[][]) second);
-		}
-		if (first instanceof Double[][] && second instanceof Double[][]) {
-			this.isEqual = equals( (Double[][]) first, (Double[][]) second);
-		}
-		if (first instanceof String[][] && second instanceof String[][]) {
-			this.isEqual = equals( (String[][]) first, (String[][]) second);
-		}
-
-		this.isEqual = first.equals(second);
-	}
-	
 	public boolean getIsEqual() {
-		return this.isEqual;
-	}
-
-	//Equals methods
-	public boolean equals(Integer first, Integer second) {
-		if (first != second) return false;
-		return true;
-	}
-	public boolean equals(Double first, Double second) {
-		if (first != second) return false;
-		return true;
-	}	
-	public boolean equals(String first, String second) {
-		if (!first.equals(second)) return false;
-		return true;
-	}
-	public boolean equals(Integer[] first, Integer[] second) {
-		if (first.length != second.length) {
-			return false;
+		if (first.getClass().isArray() && second.getClass().isArray()) {
+			return Arrays.deepEquals((Object[]) first, (Object[]) second);
+		} else {
+			return first.equals(second);
 		}
-		for (int i = 0; i < second.length; i++) {
-			if (first[i] != second[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
-	public boolean equals(Double[] first, Double[] second) {
-		if (first.length != second.length) {
-			return false;
-		}
-		for (int i = 0; i < second.length; i++) {
-			if (first[i] != second[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
-	public boolean equals(String[] first, String[] second) {
-		if (first.length != second.length) {
-			return false;
-		}
-		for (int i = 0; i < second.length; i++) {
-			if (first[i] != second[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
-	public boolean equals(Integer[][] first, Integer[][] second) {
-		if (first.length != second.length) return false;
-		for (int i = 0; i < first.length; i++) {
-			if (first[i].length != second[i].length) return false;
-			for (int j = 0; j < first[i].length; j++) {
-				if (first[i][j] != second[i][j]) return false;
-			}
-		}
-		return true;
-	}
-	public boolean equals(Double[][] first, Double[][] second) {
-		if (first.length != second.length) return false;
-		for (int i = 0; i < first.length; i++) {
-			if (first[i].length != second[i].length) return false;
-			for (int j = 0; j < first[i].length; j++) {
-				if (first[i][j] != second[i][j]) return false;
-			}
-		}
-		return true;
-	}
-	public boolean equals(String[][] first, String[][] second) {
-		if (first.length != second.length) return false;
-		for (int i = 0; i < first.length; i++) {
-			if (first[i].length != second[i].length) return false;
-			for (int j = 0; j < first[i].length; j++) {
-				if ( first[i][j].equals(second[i][j]) ) return false;
-			}
-		}
-		return true;
 	}
 }
